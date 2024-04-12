387d9cc1b16e20353e420a0599a32687d517fc9d
Petrik Diak <diak@petrik.hu>
2024-04-12 10:26:12 +0200

    ora vege

--- canvas_alapok.js
+++ canvas_alapok.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-    
+document.addEventListener("DOMContentLoaded", () => {
+    /**
+     * @type {HTMLCanvasElement}
+     */
+    const rajzlap = document.getElementById("rajzlap");
+    const rajzfelulet = rajzlap.getContext("2d");
+    //-- négyszög rajzolás
+    rajzfelulet.fillStyle = "green";
+    rajzfelulet.fillRect(50, 50, 150, 100);
+    rajzfelulet.fillStyle = "black";
+    //-- vonal kezőpont
+    rajzfelulet.moveTo(0, 0);
+    //-- végpont koordinátái
+    rajzfelulet.lineTo(300, 200);
+    //-- összekötjük a pontokat
+    rajzfelulet.stroke();
+
+    //-- kör
+    rajzfelulet.beginPath();
+    rajzfelulet.strokeStyle = "yellow";
+    rajzfelulet.arc(30, 30, 20, 0, 2 * Math.PI);
+    rajzfelulet.stroke();
+
+    //-- szöveg megjelenítése
+    rajzfelulet.font = "20px Arial";
+    rajzfelulet.fillText("Ennyi a rajzolás!!!", 10,100);
 })
